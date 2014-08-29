0cb470415cb60fcb8a8eb5030222f92e2590f0e5
ewanferguson <ef90@le.ac.uk>
2014-08-29 10:37:43 +0100

    new methods for pick class

--- include/pick.hpp
+++ include/pick.hpp
@@ -7,12 +7,15 @@ using namespace std;
 
 class generator{
   mt19937 g1;
-int lnum;
-double store;
+  int lnum;
+  double store;
 public:
   void getblock (double *output, int n);
   generator(unsigned x);
   double getnum();
+  generator();
+  void initialize(unsigned x);
+
 };
 
 #endif
